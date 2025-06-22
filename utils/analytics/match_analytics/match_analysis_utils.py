40ca86a24c9b41f86f71e32867726394766c4ab7
BeesBarGirl25 <beesbargirl25@gmail.com>
2025-06-22 17:44:06 +0100

    Updating to aggregate before applying emojis

--- utils/analytics/match_analytics/match_analysis_utils.py
+++ utils/analytics/match_analytics/match_analysis_utils.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import logging
+
+def cumulative_stats(team_data: pd.DataFrame):
+    team_data['goals'] = team_data['shot_outcome'].apply(lambda x: 1 if x == 'Goal' else 0)
+    team_data.replace(-999, 0, inplace=True)
+    team_data = team_data.sort_values('minute')
+    team_data['cum_goals'] = team_data['goals'].cumsum()
+    team_data['cum_xg'] = team_data['shot_statsbomb_xg'].cumsum()
+    return team_data
+
+import pandas as pd
 
 def extract_player_names(row):
     return [player['player']['name'] for player in row['lineup']]
